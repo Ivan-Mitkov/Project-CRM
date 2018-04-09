6ec63e8c133bae012ebe210370b8b6225be446af
Ivan Mitkov <ivan.mitkov@gmail.com>
2018-04-09 14:01:37 +0300

    account view

--- src/main/java/com/example/demo/converters/accountConverters/AccountToAccountViewModel.java
+++ src/main/java/com/example/demo/converters/accountConverters/AccountToAccountViewModel.java
@@ -1,19 +1,38 @@
 package com.example.demo.converters.accountConverters;
 
 import org.springframework.core.convert.converter.Converter;
+import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Component;
 
+import com.example.demo.converters.adressConverters.AdressToAdressViewModel;
 import com.example.demo.entities.Account;
 import com.example.demo.viewmodel.AccountViewModel;
 
 @Component
 public class AccountToAccountViewModel implements 
 Converter<Account,AccountViewModel > {
+	private final AdressToAdressViewModel converter;
+	
 
+	public AccountToAccountViewModel(AdressToAdressViewModel converter) {
+		this.converter = converter;
+	}
+
+
+	@Nullable
 	@Override
 	public AccountViewModel convert(Account source) {
-		// TODO Auto-generated method stub
-		return null;
+		if(source==null) {
+			return null;
+		}
+		final AccountViewModel account=new AccountViewModel();
+		account.setName(source.getName());
+		account.setAdress(converter.convert(source.getAdress()));
+		account.setEmail(source.getEmail());
+		account.setPhone(source.getPhone());
+		return account;
+		
 	}
+
 
 }
--- src/main/java/com/example/demo/converters/accountConverters/AccountViewModelToAccount.java
+++ src/main/java/com/example/demo/converters/accountConverters/AccountViewModelToAccount.java
@@ -1,19 +1,78 @@
 package com.example.demo.converters.accountConverters;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
 
+import com.example.demo.converters.adressConverters.AdressBindingModelToAdress;
+import com.example.demo.converters.adressConverters.AdressViewModelToAdress;
 import com.example.demo.entities.Account;
+import com.example.demo.entities.Adress;
+import com.example.demo.entities.Contact;
+import com.example.demo.entities.Employee;
+import com.example.demo.entities.Event;
+import com.example.demo.entities.Manager;
+import com.example.demo.entities.Opportunity;
+import com.example.demo.entities.Sale;
+import com.example.demo.entities.Task;
+import com.example.demo.serviceImpl.AccountServiceImpl;
 import com.example.demo.viewmodel.AccountViewModel;
 
 @Component
 public class AccountViewModelToAccount implements 
 Converter<AccountViewModel,Account >{
+	private final AdressViewModelToAdress converter;
+	private final AccountServiceImpl service;
+	
+	
+
+	public AccountViewModelToAccount(AdressViewModelToAdress converter, AccountServiceImpl service) {
+		this.converter = converter;
+		this.service = service;
+	}
 
 	@Override
 	public Account convert(AccountViewModel source) {
-		// TODO Auto-generated method stub
-		return null;
+		if(source==null) {
+			return null;
+		}
+		String name=source.getName();
+		Optional<Account> accountOptional =service.findAccountByName(name);
+		Account account=new Account();
+		if(accountOptional.isPresent()) {
+			account=accountOptional.get();
+		}
+		
+		else {
+			
+			 List<Contact> contacts=new ArrayList<>();
+			 account.setContacts(contacts);
+			 List<Event> events=new ArrayList<>();
+			 account.setEvents(events);
+			 List<Manager> managers=new ArrayList<>();
+			 account.setManagersWorkingForThisAccount(managers);
+			 List<Employee> employes=new ArrayList<>();
+			 account.setWorkingForThisAccount(employes);
+			 List<Task> tasks=new ArrayList<>();
+			 account.setTasks(tasks);
+			 List<Opportunity> opportunities=new ArrayList<>();
+			 account.setOpportunities(opportunities);
+			 List<Sale> sales=new ArrayList<>();
+			 account.setSalesForThisAccount(sales);
+			 Adress adress=new Adress();
+			 account.setAdress(adress);
+		}
+		
+		account.setAdress(converter.convert(source.getAdress()));
+		account.setEmail(source.getEmail());
+		account.setName(source.getName());
+		account.setPhone(source.getPhone());
+		
+		
+		return account;
 	}
 
 }
