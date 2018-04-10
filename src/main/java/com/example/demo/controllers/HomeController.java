6cd8f83bdcb6cd8d22e15019b69d3a0f7cc68647
Ivan Mitkov <ivan.mitkov@gmail.com>
2018-04-10 20:36:01 +0300

    index page

---
+++ src/main/java/com/example/demo/controllers/HomeController.java
@@ -0,0 +1,23 @@
+package com.example.demo.controllers;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+	@Controller
+	 @RequestMapping({"", "/", "/index"})
+	public class HomeController {
+		Logger logger=LoggerFactory.getLogger(HomeController.class);
+		 @RequestMapping({"", "/", "/index"})
+		 public String getIndexPage(Model model) {
+		        logger.info("Inside home controller");
+
+		        return "index";
+		    }
+
+}
